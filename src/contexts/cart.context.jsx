085415032e65b12bd7ada922ef63edfd97a75fb0
marcusfu <marcussfu@gmail.com>
2022-12-30 14:47:39 +0800

    add cart count

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -28,16 +28,20 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},
     cardItems: [],
     addItemToCart: () => {},
+    cartCount: 0,
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
     
-    const addItemToCart = (productToAdd) => 
+    const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
+        setCartCount(cartCount+1);
+    };
 
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount};
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
