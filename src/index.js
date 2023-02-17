a878a390a125fe0feb5d32a2892083c633358408
marcusfu <marcussfu@gmail.com>
2023-02-17 16:30:34 +0800

    adjust category item

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import {UserProvider} from './contexts/user.context';
-import {ProductsProvider} from './contexts/products.context';
+import {CategoriesProvider} from './contexts/categories.context';
 import {CartProvider} from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
