639354b5f5e8e40cf5257b608d3318c28c30f5d8
marcusfu <marcussfu@gmail.com>
2023-02-20 13:50:37 +0800

    adjust card-icon and card-dropdown and directory-item

--- src/components/card-icon/card-icon.component.jsx
+++ src/components/card-icon/card-icon.component.jsx
@@ -1,14 +1,14 @@
 import {useContext} from 'react';
 import {CartContext} from '../../contexts/cart.context';
 
-import {CartIconContainer, CartIconShoppingIcon, CartIconItemCount} from './card-icon.styles';
+import {CartIconContainer, ShoppingIcon, CartIconItemCount} from './card-icon.styles';
 
 const CardIcon = () => {
     const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
     return (
         <CartIconContainer onClick={toggleIsCartOpen}>
-            <CartIconShoppingIcon />
+            <ShoppingIcon />
             <CartIconItemCount>{cartCount}</CartIconItemCount>
         </CartIconContainer>
     )
